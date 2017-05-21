c90eb64cf950f7ba1e140b38413a34b6f19e3017
Trevor Gowing <trev.gowing@gmail.com>
2017-05-21 11:00:59 +0200

    Constructors, equals, hashCode and toString

--- src/main/java/com/trevorgowing/UrlStringBuilder.java
+++ src/main/java/com/trevorgowing/UrlStringBuilder.java
@@ -1,10 +1,43 @@
 package com.trevorgowing;
 
+import java.util.Objects;
+
 public class UrlStringBuilder {
 
     private final StringBuilder urlBuilder;
 
-    public UrlStringBuilder() {
+    private UrlStringBuilder() {
         this.urlBuilder = new StringBuilder();
+    }
+
+    private UrlStringBuilder(String baseUrl) {
+        this();
+        urlBuilder.append(baseUrl);
+    }
+
+    public static UrlStringBuilder emptyUrlBuilder() {
+        return new UrlStringBuilder();
+    }
+
+    public static UrlStringBuilder basedUrlBuilder(String baseUrl) {
+        return new UrlStringBuilder(baseUrl);
+    }
+
+    @Override
+    public boolean equals(Object objectToCompareTo) {
+        if (this == objectToCompareTo) return true;
+        if (objectToCompareTo == null || getClass() != objectToCompareTo.getClass()) return false;
+        UrlStringBuilder urlStringBuilderToCompareTo = (UrlStringBuilder) objectToCompareTo;
+        return Objects.equals(urlBuilder.toString(), urlStringBuilderToCompareTo.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(urlBuilder.toString());
+    }
+
+    @Override
+    public String toString() {
+        return urlBuilder.toString();
     }
 }
--- src/test/java/com/trevorgowing/UrlStringBuilderTests.java
+++ src/test/java/com/trevorgowing/UrlStringBuilderTests.java
@@ -1,8 +1,64 @@
 package com.trevorgowing;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static com.trevorgowing.UrlStringBuilder.basedUrlBuilder;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 @RunWith(JUnit4.class)
 public class UrlStringBuilderTests {
+
+    private static final String BASE_URL = "http://trevorgowing.com";
+
+    @Test
+    public void testConstructUrlStringBuilderWithBaseUrl_shouldSetUrlToBaseUrl() {
+        // Exercise SUT
+        String actualUrl = basedUrlBuilder(BASE_URL).toString();
+
+        // Verify state
+        assertThat(actualUrl, is(BASE_URL));
+    }
+
+    @Test
+    public void testEqualsWithDifferentUrls_shouldNotBeEqual() {
+        // Set up fixture
+        UrlStringBuilder urlStringBuilderOne = basedUrlBuilder(BASE_URL);
+        UrlStringBuilder urlStringBuilderTwo = basedUrlBuilder("http://someotherdomain.com");
+
+        // Verify state
+        assertThat(urlStringBuilderOne.equals(urlStringBuilderTwo), is(false));
+    }
+
+    @Test
+    public void testEqualsWithTheSameUrls_shouldBeEqual() {
+        // Set up fixture
+        UrlStringBuilder urlStringBuilderOne = basedUrlBuilder(BASE_URL);
+        UrlStringBuilder urlStringBuilderTwo = basedUrlBuilder(BASE_URL);
+
+        // Verify state
+        assertThat(urlStringBuilderOne.equals(urlStringBuilderTwo), is(true));
+    }
+
+    @Test
+    public void testHashCodeWithDifferentUrls_shouldNotBeEqual() {
+        // Set up fixture
+        UrlStringBuilder urlStringBuilderOne = basedUrlBuilder(BASE_URL);
+        UrlStringBuilder urlStringBuilderTwo = basedUrlBuilder("http://someotherdomain.com");
+
+        // Verify state
+        assertThat(urlStringBuilderOne.hashCode(), is(not(equalTo(urlStringBuilderTwo.hashCode()))));
+    }
+
+    @Test
+    public void testHashCodeWithTheSameUrls_shouldBeEqual() {
+        // Set up fixture
+        UrlStringBuilder urlStringBuilderOne = basedUrlBuilder(BASE_URL);
+        UrlStringBuilder urlStringBuilderTwo = basedUrlBuilder(BASE_URL);
+
+        // Verify state
+        assertThat(urlStringBuilderOne.hashCode(), is(equalTo(urlStringBuilderTwo.hashCode())));
+    }
 }
