0b9b5d9c1a5ac1e5cd41582d4d55a01f86236ea0
Trevor Gowing <trev.gowing@gmail.com>
2017-05-21 10:17:25 +0200

    Bootstrap with gradle

---
+++ src/main/java/com/trevorgowing/UrlStringBuilder.java
@@ -0,0 +1,10 @@
+package com.trevorgowing;
+
+public class UrlStringBuilder {
+
+    private final StringBuilder urlBuilder;
+
+    public UrlStringBuilder() {
+        this.urlBuilder = new StringBuilder();
+    }
+}
